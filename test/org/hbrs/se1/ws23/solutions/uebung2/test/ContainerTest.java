ce36ddd30125f0f9c274ed09d962c799b1d24809
Unknxxwn <36553113+Unknxxwn@users.noreply.github.com>
2023-11-10 09:41:48 +0100

    another push for chages

--- test/org/hbrs/se1/ws23/solutions/uebung2/test/ContainerTest.java
+++ test/org/hbrs/se1/ws23/solutions/uebung2/test/ContainerTest.java
@@ -63,14 +63,13 @@ class ContainerTest {
         String ergebnis = container.deleteMember(999); // ID nicht vorhanden!
         assertEquals(2, container.size(), "Testfall 6 - Pruefung auf Zustand 2");
         // Zudem ok zu testen:
-        assertEquals( "Member nicht enthalten - ERROR" , ergebnis );
+        assertEquals("Member nicht enthalten - ERROR", ergebnis);
 
         container.size(); // Size darf keinen Zustandswechsel auslösen!
         assertEquals(2, container.size(), "Testfall 7 - Pruefung auf Zustand 2");
 
         container.dump(); // Dump darf keinen Zustandswechsel auslösen!
         assertEquals(2, container.size(), "Testfall 8 - Pruefung auf Zustand 2");
-
 
         // Testfälle 9 - 10. Zurück auf Zustand 0
         container.deleteMember(12);
@@ -82,19 +81,20 @@ class ContainerTest {
 
     @Test
     public void testAufNullVerschiedeneWege() {
-            // Test auf NULL - der "altbekannte" Weg
-            try {
-                container.addMember(null);
-            } catch (ContainerException e) {
-                assertEquals( "NULL-Werte dürfen nicht aufgenommen werden!" ,
-                        e.getMessage() );
-            }
-            assertEquals(0 , container.size() ) ;
-
-            // Test auf NULL - der "moderne" Weg
-            Exception e = assertThrows( ContainerException.class, () -> { container.addMember(null); } );
-            assertEquals(0 , container.size() ) ;
+        // Test auf NULL - der "altbekannte" Weg
+        try {
+            container.addMember(null);
+        } catch (ContainerException e) {
+            assertEquals("NULL-Werte dürfen nicht aufgenommen werden!",
+                    e.getMessage());
         }
+        assertEquals(0, container.size());
 
+        // Test auf NULL - der "moderne" Weg
+        Exception e = assertThrows(ContainerException.class, () -> {
+            container.addMember(null);
+        });
+        assertEquals(0, container.size());
+    }
 
 }
